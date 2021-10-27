d208aaf928f0e7ab8e720f0a1742ad671b95fb35
Gonzalo Antonio Sosa Malaga <gonzasosa@outlook.com>
2021-10-27 13:31:00 -0500

    Improvements

--- BasicServiceApp/app/src/main/java/mx/uv/fiee/iinf/basicserviceapp/MainActivity.java
+++ BasicServiceApp/app/src/main/java/mx/uv/fiee/iinf/basicserviceapp/MainActivity.java
@@ -1,8 +1,13 @@
 package mx.uv.fiee.iinf.basicserviceapp;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
@@ -14,8 +19,10 @@ public class MainActivity extends Activity {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_main);
 
-        findViewById(R.id.startService).setOnClickListener (view -> {
+        findViewById (R.id.startService).setOnClickListener (view -> {
             intent = new Intent (getBaseContext (), MyOsomService.class);
+            intent.putExtra ("TIME", System.currentTimeMillis ());
+
             startService (intent);
         });
 
@@ -23,4 +30,30 @@ public class MainActivity extends Activity {
             if (intent != null) stopService (intent);
         });
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        registerReceiver (receiver, new IntentFilter ("MYINTENTFILTER"));
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if (receiver != null) {
+            unregisterReceiver (receiver);
+        }
+    }
+
+    BroadcastReceiver receiver = new BroadcastReceiver () {
+        @Override
+        public void onReceive (Context context, Intent intent) {
+            Log.d ("TAG", "Receiving...");
+            if (intent == null) return;
+
+            long timeFinish = intent.getLongExtra ("FINISH", 0);
+            Toast.makeText (getBaseContext(), "Finish time is " + timeFinish, Toast.LENGTH_LONG).show();
+        }
+    };
 }
--- BasicServiceApp/app/src/main/java/mx/uv/fiee/iinf/basicserviceapp/MyOsomService.java
+++ BasicServiceApp/app/src/main/java/mx/uv/fiee/iinf/basicserviceapp/MyOsomService.java
@@ -7,6 +7,7 @@ import android.util.Log;
 
 import androidx.annotation.Nullable;
 
+// foreground service
 public class MyOsomService extends Service {
     public static final String TAG = "MyAwesomeService";
 
@@ -26,8 +27,24 @@ public class MyOsomService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.d (TAG, "onStartCommand");
+
+        long startTime = intent.getLongExtra ("TIME", 0);
+        doDownload (startTime);
+
         return START_STICKY;
         //return super.onStartCommand (intent, flags, startId);
+    }
+
+    // long process
+    void doDownload (long time) {
+        Log.d ("TAG", "On download method...");
+        try {
+            Thread.sleep(5000); // detener el hilo
+        } catch (Exception ex) { ex.printStackTrace (); }
+
+        Intent intent = new Intent ("MYINTENTFILTER"); //package result
+        intent.putExtra ("FINISH", System.currentTimeMillis ());
+        sendBroadcast (intent);
     }
 
     @Override
